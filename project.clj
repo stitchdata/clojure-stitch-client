8b2bd4ee0811a52f1df3cade27709cac08c3c12c
Mike DeLaurentis <mdelaurentis@rjmetrics.com>
2016-08-12 10:27:18 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-stitch-client "0.1.1"
+(defproject clojure-stitch-client "0.1.2-SNAPSHOT"
   :description "Clojure Stitch Client"
   :url "http://example.com/FIXME"
   :license {:name "Apache License, Version 2.0"
