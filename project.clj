b0b65171e76e23a165e4595905e020250809ea5c
Paul Santa Clara <psantaclara@rjmetrics.com>
2016-10-19 11:48:51 -0400

    bumping java-stitch-client dep

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.stitchdata/java-stitch-client "0.4.5"]])
+                 [com.stitchdata/java-stitch-client "0.4.11"]])
