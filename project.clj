3f15ba19c9f5a82158053eee9f6785c0d082eb11
Mike DeLaurentis <miked@stitchdata.com>
2016-12-12 10:55:38 -0500

    Version number

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject com.stitchdata/clojure-stitch-client "0.3.1"
+(defproject com.stitchdata/clojure-stitch-client "0.5.0-SNAPSHOT"
   :description "Clojure Stitch Client"
   :url "https://github.com/stitchdata/clojure-stitch-client"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.stitchdata/java-stitch-client "0.4.11"]])
+                 [com.stitchdata/java-stitch-client "0.5.3"]])
